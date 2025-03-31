9f13587a3fd1bd84fd46fb1f6e563577f58e69ae
Devendra-19 <devendrasingh00040@gmail.com>
2025-03-31 13:54:04 +0530

    init

---
+++ backend/YEP/src/main/java/com/MLM/YEP/YepApplication.java
@@ -0,0 +1,13 @@
+package com.MLM.YEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YepApplication.class, args);
+	}
+
+}
